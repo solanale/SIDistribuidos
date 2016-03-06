f573f84c30a9a19e8d1fb64103c8c9ad1c7e2c9a
Naxsel <asolanas93@gmail.com>
2016-03-06 11:30:37 +0100

    URL fixed

--- src/p1/parser.java
+++ src/p1/parser.java
@@ -5,24 +5,29 @@ import java.net.*;
 import java.text.ParseException;
 import java.util.Scanner;
 
-public class Parser {
+class Parser {
 
-    final int empresa = 35;
+    static final int empresa = 35;
 
     // Aplica un parser al html y devuelve la estructura Nombre\tUltimo\n de las 35 empresas
-    public static void main (String[] args) throws ParseException, TokenMgrError{
+    public static void main (String[] args) throws ParseException{
         URL url;
-        InputStream is = null;
         BufferedReader br;
         int i,input,ultimo;
         String text;
-        String aux [] = new String [empresa];
+        String aux [] = new String[empresa];
         String lines [][] = new String [empresa][2];
         try {
-            url = new URL("http://www.infobolsa.es/acciones/ibex35");
-            is = url.openStream();
-            br = new BufferedReader(new InputStreamReader(is));
-            text = parser.Start(br);
+//            url = new URL("http://www.infobolsa.es/acciones/ibex35");
+//            URLConnection connection = url.openConnection();
+//            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//            System.out.println(br.readLine());
+//            System.out.println(br.readLine().toString());
+
+            String out = new Scanner(new URL("http://www.infobolsa.es/acciones/ibex35").openStream(), "UTF-8").useDelimiter("\\A").next();
+            System.out.println(out);
+
+            text =""; // parser.Start(br);
             aux = text.split("\n");
             System.out.println("Introduce Numero de Empresa a consultar, 1-35");
             for (i=0; i< empresa; i++ ){
@@ -31,7 +36,7 @@ public class Parser {
             }
             Scanner in = new Scanner (System.in);
             input = in.nextInt();
-            br.close();
+            //br.close();
             br = new BufferedReader(new FileReader("p1/last.txt"));
             for (i=0; i< input; i++){
                 text = br.readLine();
@@ -42,17 +47,17 @@ public class Parser {
             System.out.printf("Empresa %s, ultimo: %d anterior %d",lines[input][0], lines[input][1], ultimo );
 
         } catch (MalformedURLException mue) {
-        mue.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+            mue.printStackTrace();
         } catch (FileNotFoundException fnf){
             fnf.printStackTrace();
-        } finally {
-            try {
-                if (is != null) is.close();
-            } catch (IOException ioe) {
-                // nothing to see here
-            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+//        } finally {
+//            try {
+//                if (is != null) is.close();
+//            } catch (IOException ioe) {
+//                // nothing to see here
+//            }
         }
     }
 }
