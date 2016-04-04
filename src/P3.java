9d730410b47dc9e3f41eb5fd031debbabc2ae42b
naxsel <asolanas93@gmail.com>
2016-04-05 01:00:24 +0200

    parte 1 acabada

--- src/P3.java
+++ src/P3.java
@@ -1,29 +1,36 @@
 /**
  * Created by naxsel on 4/04/16.
  */
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.*;
+
 public class P3
 {
     public static void main(String[] args)
     {
 
-        String tendencias[] = {"Neymar"}; // "Iago_Aspas", "Ilegales"};
+        //BasicConfigurator.configure();
+        String tendencias[] = {"Neymar", "Iago_Aspas", "Ilegales"};
         for (String tendencia : tendencias)
         {
             String sparqlEndpoint = "http://dbpedia.org/sparql";
 
-            String sparqlQuery = "SELECT ?name ?currentclub WHERE { ?x <http://live.dbpedia.org/page/"+tendencia + "> ?x }";
+            String sparqlQuery = "PREFIX res: <http://dbpedia.org/resource/>" +
+                    "PREFIX p: <http://dbpedia.org/property/>" +
+                    "PREFIX o: <http://dbpedia.org/ontology/>" +
+                    "SELECT * {" +
+                    "res:" +tendencia+ " p:name ?Nombre ." +
+                    "OPTIONAL {res:" +tendencia+ " p:currentclub ?Club } ." +
+                    "OPTIONAL {res:" +tendencia+ " o:genre ?Genero} ."+
+                    "}";
+
             System.out.println(sparqlQuery);
             org.apache.jena.query.Query query = QueryFactory.create(sparqlQuery);
             QueryExecution exec = QueryExecutionFactory.sparqlService(
                     sparqlEndpoint, query );
             try {
                 ResultSet results = exec.execSelect();
-                while ( results.hasNext() )
-                    System.out.println(results.nextSolution());
+                ResultSetFormatter.out(System.out, results, query);
+
             } finally {
                 exec.close();
             }
