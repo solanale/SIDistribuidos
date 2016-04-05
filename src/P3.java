cd292cb1d67efc0fdf21239131f7e14fa621e194
naxsel <asolanas93@gmail.com>
2016-04-05 16:30:36 +0200

    100%

--- src/P3.java
+++ src/P3.java
@@ -1,7 +1,3 @@
-/**
- * Created by naxsel on 4/04/16.
- */
-
 import org.apache.jena.query.*;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -17,46 +13,49 @@ public class P3
         ArrayList<String> tendencias  = new ArrayList<String>();
         tendencias.add("Neymar"); tendencias.add("Iago_Aspas"); tendencias.add("Ilegales");
 
-        Twitter twitter = new TwitterFactory().getInstance();
-        ResponseList<Location> locations;
-        locations = twitter.getAvailableTrends();
-        int woeid = 0;
-        for (Location location: locations){
-            if(location.getCountryName().equals("Spain"))  woeid = location.getWoeid();
-        }
-        Trends trends = twitter.getPlaceTrends(woeid);
-        for (int i = tendencias.size(); i < trends.getTrends().length;i++){
-            tendencias.add(trends.getTrends()[i].getName().trim().replace("#","").replace(" ","_"));
-            //System.out.println(tendencias.get(i));
-        }
-
-        //String tendencias[] = {"Neymar", "Iago_Aspas", "Ilegales", "Beatles"};
-        for (String tendencia : tendencias)
-        {
-            String sparqlEndpoint = "http://dbpedia.org/sparql";
-
-            String sparqlQuery = "" +
-                    "PREFIX res: <http://dbpedia.org/resource/>" +
-                    "PREFIX p: <http://dbpedia.org/property/>" +
-                    "PREFIX o: <http://dbpedia.org/ontology/>" +
-                    "SELECT * {" +
-                    "res:" +tendencia+ " p:name ?Nombre ." +
-                    "OPTIONAL {res:" +tendencia+ " p:currentclub ?Club } ." +
-                    "OPTIONAL {res:" +tendencia+ " o:genre ?Genero} ."+
-                    "}";
-
-            org.apache.jena.query.Query query = QueryFactory.create(sparqlQuery);
-            QueryExecution exec = QueryExecutionFactory.sparqlService(
-                    sparqlEndpoint, query );
-            try {
-                ResultSet results = exec.execSelect();
-                if(results.hasNext()) {
-                    ResultSetFormatter.out(System.out, results, query);
-                }
-
-            } finally {
-                exec.close();
+        //Error twitter4j.properties
+        try {
+            Twitter twitter = new TwitterFactory().getInstance();
+            ResponseList<Location> locations;
+            locations = twitter.getAvailableTrends();
+            int woeid = 0;
+            for (Location location : locations) {
+                if (location.getCountryName().equals("Spain")) woeid = location.getWoeid();
             }
+            Trends trends = twitter.getPlaceTrends(woeid);
+            for (int i = tendencias.size(); i < trends.getTrends().length; i++) {
+                tendencias.add(trends.getTrends()[i].getName().trim().replace("#", "").replace(" ", "_"));
+            }
+
+            //String tendencias[] = {"Neymar", "Iago_Aspas", "Ilegales", "Beatles"};
+            for (String tendencia : tendencias) {
+                String sparqlEndpoint = "http://dbpedia.org/sparql";
+
+                String sparqlQuery = "" +
+                        "PREFIX res: <http://dbpedia.org/resource/>" +
+                        "PREFIX p: <http://dbpedia.org/property/>" +
+                        "PREFIX o: <http://dbpedia.org/ontology/>" +
+                        "SELECT * {" +
+                        "res:" + tendencia + " p:name ?Nombre ." +
+                        "OPTIONAL {res:" + tendencia + " p:currentclub ?Club } ." +
+                        "OPTIONAL {res:" + tendencia + " o:genre ?Genero} ." +
+                        "}";
+
+                org.apache.jena.query.Query query = QueryFactory.create(sparqlQuery);
+                QueryExecution exec = QueryExecutionFactory.sparqlService(
+                        sparqlEndpoint, query);
+                try {
+                    ResultSet results = exec.execSelect();
+                    if (results.hasNext()) {
+                        ResultSetFormatter.out(System.out, results, query);
+                    }
+
+                } finally {
+                    exec.close();
+                }
+            }
+        }catch (java.lang.IllegalStateException e){
+            System.out.println("El fichero twitter4j.properties es necesario para funcionar, debe incluirse en la raiz del .jar");
         }
     }
 }
